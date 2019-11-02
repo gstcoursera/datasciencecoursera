76c42bcebc98f65353597d9ab17b837ebc169bbe
gstcoursera <g.stamatopoulos52@gmail.com>
2019-11-02 17:00:08 +0200

    Initial Commit

---
+++ load_dataset.R
@@ -0,0 +1,20 @@
+## Load dataset
+filename <- "./household_power_consumption.txt"
+data <- read.table(filename, header = TRUE, sep = ";", colClasses = c("character", "character", rep("numeric",7)), na = "?")
+dim(data) # Check if dataset has 2,075,259 rows and 9 columns
+
+## We will only be using data from the dates 2007-02-01 and 2007-02-02
+attach(data)
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+
+## Create the new dataset with only these dates
+newdata <- data[subset, ]
+attach(newdata)
+dim(newdata) # values should be 2880 rows and 9 columns
+
+## Add a new column to convert the Date and Time variables to Date/Time classes
+x <- paste(Date, Time)
+newdata$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newdata) <- 1:nrow(newdata)
+dim(newdata) # values should be 2880 rows and 10 columns
+attach(newdata)
